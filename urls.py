8647ce8ec5a4f45d633dc3966310df731aacbb0d
Subaksha B <162463871+Subaksha21@users.noreply.github.com>
2024-11-05 20:56:57 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,25 +1,23 @@
-"""
-URL configuration for counseling_system project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('counseling.urls')),
-    path('accounts/', include('allauth.urls')),
-]
+    path('', views.home, name='home'),  # Home page
+    path('login/', views.login_view, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('schedule/', views.schedule_appointment, name='schedule_appointment'),
+    path('appointments/', views.appointment_list, name='appointment_list'),
+    path('edit-appointments/', views.edit_appointment, name='edit_appointments'),
+
+    path('contact_support/', views.contact_support, name='contact_support'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('materials/', views.materials, name='materials'),
+    path('materials/download/<int:material_id>/', views.download_material, name='download_material'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
